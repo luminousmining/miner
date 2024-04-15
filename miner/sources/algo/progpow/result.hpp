162ab63b7358f87eea1c9bc7e2cde3882c0f5b7c
luminousmining <luminousminer@gmail.com>
2024-04-15 15:07:40 +0200

    add more informations

--- miner/sources/algo/progpow/result.hpp
+++ miner/sources/algo/progpow/result.hpp
@@ -19,7 +19,7 @@ namespace algo
             bool     found;
             uint32_t count;
             uint64_t nonces[4];
-            uint32_t hash[algo::progpow::MAX_RESULT][algo::LEN_HASH_256_WORD_32];
+            uint32_t hash[algo::progpow::MAX_RESULT][8/*algo::LEN_HASH_256_WORD_32*/];
         };
 
 #if !defined(__LIB_CUDA)
@@ -29,7 +29,7 @@ namespace algo
             bool        found { false };
             uint32_t    count { 0u };
             uint64_t    nonces[4] { 0ull, 0ull, 0ull, 0ull };
-            uint32_t    hash[algo::progpow::MAX_RESULT][algo::LEN_HASH_256_WORD_32]
+            uint32_t    hash[algo::progpow::MAX_RESULT][8/*algo::LEN_HASH_256_WORD_32*/]
                             {
                                 {0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u},
                                 {0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u},
