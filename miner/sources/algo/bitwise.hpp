e7aec5de63ed1626841c36cd736923b08b473a4a
luminousmining <luminousminer@gmail.com>
2024-09-08 14:28:24 +0200

    default is little endiannes

--- miner/sources/algo/bitwise.hpp
+++ miner/sources/algo/bitwise.hpp
@@ -11,23 +11,21 @@
 
 
 #if defined(_WIN32) && !defined(__BYTE_ORDER__)
-constexpr std::int32_t __ORDER_LITTLE_ENDIAN__ { 1234 };
-constexpr std::int32_t __ORDER_BIG_ENDIAN__    { 4321 };
-constexpr std::int32_t __BYTE_ORDER__          { __ORDER_LITTLE_ENDIAN__ };
+    constexpr std::int32_t __ORDER_LITTLE_ENDIAN__ { 1234 };
+    constexpr std::int32_t __ORDER_BIG_ENDIAN__    { 4321 };
+    constexpr std::int32_t __BYTE_ORDER__          { __ORDER_LITTLE_ENDIAN__ };
 #elif (defined(__linux__) || defined(__GNUC__)) && !defined(__BYTE_ORDER__)
-#error "__GNUC__ should define __BYTE_ORDER__"
-#elif !defined(__BYTE_ORDER__)
-#error "Unknow endianness"
+    #error "__GNUC__ should define __BYTE_ORDER__"
 #endif
 
 #if defined(__GNUC__)
-#define bswap32 __builtin_bswap32
-#define bswap64 __builtin_bswap64
+    #define bswap32 __builtin_bswap32
+    #define bswap64 __builtin_bswap64
 #elif defined(_MSC_VER)
-#include <string.h>
-#define __builtin_memcpy memcpy
-#define bswap32 _byteswap_ulong
-#define bswap64 _byteswap_uint64
+    #include <string.h>
+    #define __builtin_memcpy memcpy
+    #define bswap32 _byteswap_ulong
+    #define bswap64 _byteswap_uint64
 #endif
 
 
