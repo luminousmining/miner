97de8d22a0ff8704112cf94d3e09af861bbb4dee
luminousminer <luminousminer@gmail.com>
2026-01-24 10:30:59 +0100

    remove include copy_u4

--- sources/algo/progpow/cuda/progpow.cu
+++ sources/algo/progpow/cuda/progpow.cu
@@ -8,7 +8,6 @@
 #include <resolver/nvidia/progpow_kernel_parameter.hpp>
 
 #include <common/cuda/be_u64.cuh>
-#include <common/cuda/copy_u4.cuh>
 #include <common/cuda/register.cuh>
 
 #include <algo/crypto/cuda/fnv1.cuh>
