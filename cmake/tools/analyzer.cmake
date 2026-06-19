42abd14766594087254f71f88a3ecd465ea8f52a
luminousminer <luminousminer@gmail.com>
2026-06-19 20:32:21 +0200

    cppcheck target only miner

--- cmake/tools/analyzer.cmake
+++ cmake/tools/analyzer.cmake
@@ -1,14 +1,5 @@
 if (TOOL_ANALYZER)
     find_program(CPPCHECK_EXECUTABLE cppcheck)
-    if(CPPCHECK_EXECUTABLE)
-        set(CMAKE_CXX_CPPCHECK
-            ${CPPCHECK_EXECUTABLE}
-            --enable=all
-            --inconclusive
-            --std=c++20
-            --quiet
-        )
-    endif()
 
     find_program(CLANG_TIDY_EXECUTABLE
         NAMES clang-tidy-15 clang-tidy
