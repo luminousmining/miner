2bd1d5927f085e332cc08e0b4a2f1d0c1963a4f6
luminousminer <luminousminer@gmail.com>
2026-06-22 14:12:27 +0200

    refacto kHeavyHash archi

--- sources/algo/kheavyhash/tests/heavyHash.cpp
+++ sources/algo/kheavyhash/tests/heavyHash.cpp
@@ -1,12 +1,13 @@
 #include <gtest/gtest.h>
 
-#include <algo/kheavyhash/kheavyhash.hpp>
+#include <algo/kheavyhash/matrix.hpp>
+#include <algo/kheavyhash/tests/helper.hpp>
 #include <algo/kheavyhash/tests/kat_vectors.hpp>
 
 
 // Layer 2: heavy step (matrix multiply + XOR + KHeavyHash).
 // rusty-kaspa matrix.rs::test_heavy_hash literal.
-TEST(KHeavyHash, heavyHashMatchesReference)
+TEST(KHeavyHashHeavyHash, heavyHashMatchesReference)
 {
     algo::kheavyhash::Hash256 const out{
         algo::kheavyhash::heavyHash(algo::kheavyhash::kat::HEAVY_TEST_MATRIX, algo::kheavyhash::kat::HEAVY_INPUT)
@@ -16,19 +17,14 @@ TEST(KHeavyHash, heavyHashMatchesReference)
 
 
 // Layer 2: full pipeline incl. matrix generation. Oracle-minted (pycryptodome) vector.
-TEST(KHeavyHash, calculatePowMatchesReference)
+// The pow pipeline is inlined here: generateMatrix -> powHash -> heavyHash.
+TEST(KHeavyHashHeavyHash, calculatePowMatchesReference)
 {
-    algo::kheavyhash::Hash256 const out{ algo::kheavyhash::calculatePow(
+    algo::kheavyhash::Matrix const  matrix{ algo::kheavyhash::generateMatrix(algo::kheavyhash::kat::FP_PRE) };
+    algo::kheavyhash::Hash256 const hash1{ algo::kheavyhash::powHash(
         algo::kheavyhash::kat::FP_PRE,
         algo::kheavyhash::kat::FP_TIMESTAMP,
         algo::kheavyhash::kat::FP_NONCE) };
+    algo::kheavyhash::Hash256 const out{ algo::kheavyhash::heavyHash(matrix, hash1) };
     EXPECT_EQ(out, algo::kheavyhash::kat::FP_FINAL);
-}
-
-
-// Accept test: pow <= target (little-endian 256-bit).
-TEST(KHeavyHash, MeetsTargetCompare)
-{
-    EXPECT_TRUE(algo::kheavyhash::meetsTarget(algo::kheavyhash::kat::FP_FINAL, algo::kheavyhash::kat::FP_TARGET_PASS));
-    EXPECT_FALSE(algo::kheavyhash::meetsTarget(algo::kheavyhash::kat::FP_FINAL, algo::kheavyhash::kat::FP_TARGET_FAIL));
 }
--- sources/algo/kheavyhash/tests/powHash.cpp
+++ sources/algo/kheavyhash/tests/powHash.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#include <algo/kheavyhash/hashers.hpp>
+#include <algo/kheavyhash/tests/helper.hpp>
 #include <algo/kheavyhash/tests/kat_vectors.hpp>
 
 
@@ -21,19 +21,11 @@ namespace
 
 
 // Layer 2: PowHash (cSHAKE256 "ProofOfWorkHash"). Matches rusty-kaspa test_pow_hash inputs.
-TEST(KHeavyHashHashers, PowHashMatchesKat)
+TEST(KHeavyHashPowHash, PowHashMatchesKat)
 {
     algo::kheavyhash::Hash256 const pre{ toHash(algo::kheavyhash::kat::POW_KAT_PRE) };
     algo::kheavyhash::Hash256 const out{
         algo::kheavyhash::powHash(pre, algo::kheavyhash::kat::POW_KAT_TIMESTAMP, algo::kheavyhash::kat::POW_KAT_NONCE)
     };
     EXPECT_EQ(out, algo::kheavyhash::kat::POW_KAT_EXPECTED);
-}
-
-
-// Layer 2: KHeavyHash (cSHAKE256 "HeavyHash") over 32 bytes.
-TEST(KHeavyHashHashers, KHeavyHashMatchesKat)
-{
-    algo::kheavyhash::Hash256 const out{ algo::kheavyhash::kHeavyHash(algo::kheavyhash::kat::HEAVY_INPUT) };
-    EXPECT_EQ(out, algo::kheavyhash::kat::KHEAVY_EXPECTED);
 }
